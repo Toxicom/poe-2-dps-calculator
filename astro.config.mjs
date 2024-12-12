c031f5c9348b7cd0c176ec7736b1b1627f2aa5e5
Toxicom <tomas.98.leo@gmail.com>
2024-12-12 20:27:11 +0200

    build page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  integrations: [tailwind()]
+});
